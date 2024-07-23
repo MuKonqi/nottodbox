cff30d98ba471578494634e0a77f22ab6d903599
MuKonqi <mukonqi@gmail.com>
2024-07-24 00:14:47 +0300

    uncommenting on application.py

--- nottodbox/application.py
+++ nottodbox/application.py
@@ -26,23 +26,23 @@ from PyQt6.QtWidgets import QApplication
 from mainwindow import MainWindow
 
 
-# username = getpass.getuser()
-# userdata = f"/home/{username}/.local/share/nottodbox/"
-# if not os.path.isdir(userdata):
-#     os.mkdir(userdata)
+username = getpass.getuser()
+userdata = f"/home/{username}/.local/share/nottodbox/"
+if not os.path.isdir(userdata):
+    os.mkdir(userdata)
     
 
-# with sqlite3.connect(f"{userdata}settings.db", timeout=5.0) as db_settings:
-#     cur_settings = db_settings.cursor()
+with sqlite3.connect(f"{userdata}settings.db", timeout=5.0) as db_settings:
+    cur_settings = db_settings.cursor()
     
-#     sql_settings = """
-#     CREATE TABLE IF NOT EXISTS settings (
-#         setting TEXT NOT NULL PRIMARY KEY,
-#         value TEXT NOT NULL
-#     );"""
-#     cur_settings.execute(sql_settings)
+    sql_settings = """
+    CREATE TABLE IF NOT EXISTS settings (
+        setting TEXT NOT NULL PRIMARY KEY,
+        value TEXT NOT NULL
+    );"""
+    cur_settings.execute(sql_settings)
     
-#     db_settings.commit()
+    db_settings.commit()
     
 
 class Application(QApplication):
