de25eae815e59bedd8aa03986acabecb772101b1
MuKonqi <mukonqi@gmail.com>
2025-03-16 01:24:31 +0300

    Update merge.py

--- po/merge.py
+++ po/merge.py
@@ -8,15 +8,8 @@ with open("po/LINGUAS") as f:
 for lang in linguas:
     if not os.path.isdir(f"AppDir/usr/local/lib/python3.12/dist-packages/nottodbox/locale/{lang}/LC_MESSAGES"):
         os.makedirs(f"AppDir/usr/local/lib/python3.12/dist-packages/nottodbox/locale/{lang}/LC_MESSAGES")
-        
-    if not os.path.isdir(f"AppDir/usr/local/share/applications/"):
-        os.makedirs("AppDir/usr/local/share/applications/")
-        
-    if not os.path.isdir(f"AppDir/usr/local/share/metainfo/"):
-        os.makedirs("AppDir/usr/local/share/metainfo/")
     
     subprocess.run(["msgfmt", f"po/{lang}.po"])
     shutil.move("messages.mo", f"AppDir/usr/local/lib/python3.12/dist-packages/nottodbox/locale/{lang}/LC_MESSAGES/nottodbox.mo")
     
-    subprocess.run(["msgfmt", "--desktop", "--template", "share/applications/io.github.mukonqi.nottodbox.desktop.in", "-l", lang, "-o", "AppDir/usr/local/share/applications/io.github.mukonqi.nottodbox.desktop", f"po/{lang}.po"])
-    subprocess.run(["msgfmt", "--xml", "--template", "share/metainfo/io.github.mukonqi.nottodbox.metainfo.xml.in", "-l", lang, "-o", "AppDir/usr/local/share/metainfo/io.github.mukonqi.nottodbox.metainfo.xml", f"po/{lang}.po"])
+    subprocess.run(["msgfmt", "--desktop", "--template", "share/applications/io.github.mukonqi.nottodbox.desktop.in", "-l", lang, "-o", "AppDir/io.github.mukonqi.nottodbox.desktop", f"po/{lang}.po"])
