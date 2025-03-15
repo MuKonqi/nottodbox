2053712737d5083ee4b857aa9de6ac6187fbb6af
MuKonqi <mukonqi@gmail.com>
2025-03-15 23:51:48 +0300

    Update merge.py

--- po/merge.py
+++ po/merge.py
@@ -6,6 +6,6 @@ with open("po/LINGUAS") as f:
     linguas = f.readlines()
     
 for lang in linguas:
-    os.makedirs(f"AppDir/usr/local/share/nottodbox/locale/{lang}/LC_MESSAGES/")
+    os.makedirs(f"AppDir/usr/local/lib/python3.12/dist-packages/nottodbox/locale/{lang}/LC_MESSAGES")
     subprocess.run(["msgfmt", "-D", "po", f"{lang}.po"])
-    shutil.move("messages.mo", f"AppDir/usr/local/share/nottodbox/locale/{lang}/LC_MESSAGES/nottodbox.mo")
+    shutil.move("messages.mo", f"AppDir/usr/local/lib/python3.12/dist-packages/nottodbox/locale/{lang}/LC_MESSAGESnottodbox.mo")
