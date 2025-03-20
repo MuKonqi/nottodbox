752bca8088d64bd5ed50f965389014f2d512ccb3
Muhammed S. <mukonqi@gmail.com>
2025-03-20 22:34:31 +0300

    Add link to version information in about page

--- nottodbox/about.py
+++ nottodbox/about.py
@@ -44,7 +44,8 @@ class AboutWidget(QWidget):
         font.setPointSize(32)
         self.nottodbox.setFont(font)
         
-        self.version_label = Label(self, _("Version") + ': v0.0.8')
+        self.version_label = Label(self, _("Version") + ': <a href="https://github.com/mukonqi/nottodbox/releases/tag/v0.0.8">v0.0.8</a>')
+        self.version_label.setOpenExternalLinks(True)
         
         self.source_label = Label(self, _("Source codes") + ': <a href="https://github.com/mukonqi/nottodbox">GitHub</a>')
         self.source_label.setOpenExternalLinks(True)
