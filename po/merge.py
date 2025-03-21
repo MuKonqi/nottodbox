44a4530d7b361a68eeca247938ae9d0e6d29cd7f
Muhammed S. <mukonqi@gmail.com>
2025-03-22 01:01:05 +0300

    Some minor updates

--- po/merge.py
+++ po/merge.py
@@ -1,13 +1,12 @@
 import os
 import subprocess
-import shutil
 
 with open("po/LINGUAS") as f:
     linguas = f.readlines()
     
 for lang in linguas:
-    if not os.path.isdir(f"AppDir/usr/local/lib/python3.12/dist-packages/nottodbox/locale/{lang}/LC_MESSAGES"):
-        os.makedirs(f"AppDir/usr/local/lib/python3.12/dist-packages/nottodbox/locale/{lang}/LC_MESSAGES")
+    if not os.path.isdir(f"AppDir/usr/local/lib/python3.13/dist-packages/nottodbox/locale/{lang}/LC_MESSAGES"):
+        os.makedirs(f"AppDir/usr/local/lib/python3.13/dist-packages/nottodbox/locale/{lang}/LC_MESSAGES")
     
-    subprocess.run(["msgfmt", f"po/{lang}.po"])
-    shutil.move("messages.mo", f"AppDir/usr/local/lib/python3.12/dist-packages/nottodbox/locale/{lang}/LC_MESSAGES/nottodbox.mo")
+    subprocess.run(["msgfmt", "-o", f"AppDir/usr/local/lib/python3.13/dist-packages/nottodbox/locale/{lang}/LC_MESSAGES/nottodbox.mo", f"po/{lang}.po"])
+    subprocess.run(["msgfmt", "--desktop", "-l", lang, "-o", "AppDir/io.github.mukonqi.nottodbox.desktop", "--template", "share/applications/io.github.mukonqi.nottodbox.desktop.in", f"po/{lang}.po"])
