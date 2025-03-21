3f8a71ad7dc74b56a29b615c47e1bed3d0f0328d
Muhammed S. <mukonqi@gmail.com>
2025-03-22 01:40:24 +0300

    Try to reduce size of AppImage

--- po/merge.py
+++ po/merge.py
@@ -5,8 +5,8 @@ with open("po/LINGUAS") as f:
     linguas = f.readlines()
     
 for lang in linguas:
-    if not os.path.isdir(f"AppDir/usr/local/lib/python3.13/dist-packages/nottodbox/locale/{lang}/LC_MESSAGES"):
-        os.makedirs(f"AppDir/usr/local/lib/python3.13/dist-packages/nottodbox/locale/{lang}/LC_MESSAGES")
+    if not os.path.isdir(f"AppDir/usr/lib/python3.13/site-packages/nottodbox/locale/{lang}/LC_MESSAGES"):
+        os.makedirs(f"AppDir/usr/lib/python3.13/site-packages/nottodbox/locale/{lang}/LC_MESSAGES")
     
-    subprocess.run(["msgfmt", "-o", f"AppDir/usr/local/lib/python3.13/dist-packages/nottodbox/locale/{lang}/LC_MESSAGES/nottodbox.mo", f"po/{lang}.po"])
+    subprocess.run(["msgfmt", "-o", f"AppDir/usr/lib/python3.13/site-packages/nottodbox/locale/{lang}/LC_MESSAGES/nottodbox.mo", f"po/{lang}.po"])
     subprocess.run(["msgfmt", "--desktop", "-l", lang, "-o", "AppDir/io.github.mukonqi.nottodbox.desktop", "--template", "share/applications/io.github.mukonqi.nottodbox.desktop.in", f"po/{lang}.po"])
