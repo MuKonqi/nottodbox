e23e3dd35b57f05a11cfd56c37aea563477d0abf
Muhammed S. <mukonqi@gmail.com>
2025-06-29 21:35:35 +0300

    Some changes

---
+++ .github/scripts/translations.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+import sys
+
+if "--appimage" in sys.argv:
+    LOCALE_DIR = "AppDir/usr/lib/python3.13/site-packages/nottodbox/locale"
+    DESKTOP_FILE = "AppDir/usr/share/applications/io.github.mukonqi.nottodbox.desktop"
+    
+    os.makedirs("AppDir/usr/share/applications", exist_ok=True)
+    os.makedirs("AppDir/usr/share/metainfo", exist_ok=True)
+    
+else:
+    DESKTOP_FILE = "share/applications/io.github.mukonqi.nottodbox.desktop"
+    METADATA_FILE = "share/metainfo/io.github.mukonqi.nottodbox.appdata.xml"
+
+
+subprocess.run(["msgfmt", "--desktop", "-o", DESKTOP_FILE, "--template", "share/applications/io.github.mukonqi.nottodbox.desktop.in.in", "-d", "po"])
+os.chmod(DESKTOP_FILE, 0o777)
+
+subprocess.run(["msgfmt", "--xml", "-o", METADATA_FILE, "--template", "share/metainfo/io.github.mukonqi.nottodbox.appdata.xml.in", "-d", "po"])
