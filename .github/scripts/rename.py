a4972854892e0dd93c4d5e97619d6842627c579f
Muhammed S. <mukonqi@gmail.com>
2025-07-07 16:29:24 +0300

    Update "rename" script

--- .github/scripts/rename.py
+++ .github/scripts/rename.py
@@ -5,7 +5,7 @@ if os.environ.get("RUNNER_OS") == "Linux":
     directory = os.listdir()
 
     for name in directory:
-        if "appimage" in name:
+        if "appimage" in name.lower():
             shutil.move(name, "dist/nottodbox.AppImage")
 
 elif os.environ.get("RUNNER_OS") == "macOS":
