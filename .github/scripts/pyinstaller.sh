307393a59ef25f00fca3a4bb41d02230e1956cca
Muhammed S. <mukonqi@gmail.com>
2025-07-08 19:37:57 +0300

    chore: Do not use sed

--- .github/scripts/pyinstaller.sh
+++ .github/scripts/pyinstaller.sh
@@ -1,12 +1,9 @@
-if [ "$RUNNER_OS" == "Linux" ]; then
-    sed -i "s/APP_BUILD = .*/APP_BUILD = 'AppImage'/" nottodbox/scripts/version.py
-    pyinstaller --add-data 'nottodbox/color-schemes/:nottodbox/color-schemes' --add-data 'nottodbox/LICENSE.txt:nottodbox' -F -w -n nottodbox nottodbox/__main__.py
+python3 .github/scripts/consts.py
 
+if [ "$RUNNER_OS" == "Linux" ]; then
+    pyinstaller --add-data 'nottodbox/color-schemes/:nottodbox/color-schemes' --add-data 'nottodbox/LICENSE.txt:nottodbox' -F -w -n nottodbox nottodbox/__main__.py
 elif [ "$RUNNER_OS" == "macOS" ]; then
-    sed -i '' "s/APP_BUILD = .*/APP_BUILD = 'PyInstaller'/" nottodbox/scripts/version.py
-    pyinstaller --add-data 'nottodbox/color-schemes/:nottodbox/color-schemes' --add-data 'nottodbox/LICENSE.txt:nottodbox' -D -w -i share/icons/pyinstaller/io.github.mukonqi.nottodbox.icns -n nottodbox nottodbox/__main__.py
-    
+    pyinstaller --add-data 'nottodbox/color-schemes/:nottodbox/color-schemes' --add-data 'nottodbox/LICENSE.txt:nottodbox' -D -w -i share/icons/pyinstaller/io.github.mukonqi.nottodbox.icns -n nottodbox nottodbox/__main__.py 
 elif [ "$RUNNER_OS" == "Windows" ]; then
-    sed -i "s/APP_BUILD = .*/APP_BUILD = 'PyInstaller'/" nottodbox/scripts/version.py
     pyinstaller --add-data 'nottodbox/color-schemes/:nottodbox/color-schemes' --add-data 'nottodbox/LICENSE.txt:nottodbox' -F -w -i share/icons/pyinstaller/io.github.mukonqi.nottodbox.ico -n nottodbox nottodbox/__main__.py
 fi
